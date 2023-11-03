150d39aa581f88c9a3bbba6aa3162191a56f1c7d
Michell <mjaramillo@imr.com.co>
2023-11-03 09:54:16 -0500

    upload_file_to_ftp

--- apps/customers/api/functions/functions.py
+++ apps/customers/api/functions/functions.py
@@ -1,13 +1,17 @@
 
 from datetime import datetime
 from django.db import connections
+from pathlib import Path
 from rest_framework.response import Response
 
 from apps.agreement.models import Agreement
+from commons.classes import conexion_ftp
 from ..serializers.serializers import *
 
 import os
 import re
+import sys
+import traceback
 
 
 def create_client_agreement(id_agreement, customer):
@@ -67,10 +71,34 @@ def file_comerssia():
                 line = f'{document}|{quota}|{quota}|0\n'
                 file.write(line)
 
+        # upload_file_to_ftp(file_name)
+        # se comenta la linea del archivo ftp mientras se puede hacer pruebas
+
         return True 
     except Exception as e:
         error_message = f'Error al generar y guardar el archivo TXT: {str(e)}'
         return {'message': error_message}
+    
+def upload_file_to_ftp(file_name):
+    try:
+        file_path = Path(f'C:\\cargas') / file_name
+
+        ftp_comerssia = conexion_ftp()
+        ftp_comerssia = ftp_comerssia.getInstance()
+        ftp_comerssia.cwd('Interfaces/Salida')
+        ftp_comerssia.encoding='utf-8'
+        ftp_comerssia.sendcmd('OPTS UTF8 ON')
+
+        file = open(file_path,'rb')
+        ftp_comerssia.storbinary("STOR "+ file_name, file, 1024)
+
+        ftp_comerssia.quit()
+
+    except Exception as e:
+        traceback.print_exc()
+        e = sys.exc_info()[1]
+        data = {'message': str(e), 'code': 2, 'data': None}
+        return Response(data, status=200)
     
 
 def list_users():
