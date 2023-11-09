1440135eb7048ce45bb64132d0746c32a208da14
Andres <jramirez@imr.com.co>
2023-11-09 15:49:41 -0500

    Update settings.py

--- b2b/settings.py
+++ b2b/settings.py
@@ -17,12 +17,6 @@ AUTH_PROFILE_MODULE = 'users.User'
 ACCOUNT_AUTHENTICATION_METHOD = 'documento'
 ACCOUNT_USERNAME_REQUIRED = False
 
-LOGIN_URL = 'login/'
-
-LOGOUT_REDIRECT_URL = '/login'
-
-LOGIN_REDIRECT_URL = '/'
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,7 +43,6 @@ INSTALLED_APPS = [
 
 ]
 
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -64,15 +57,6 @@ MIDDLEWARE = [
     'b2b.custom_authentication_middleware.CustomAuthenticationMiddleware',
 
 ]
-
-#CORS_ORIGIN_ALLOW_ALL = True
-
-#CORS_ALLOWED_ORIGINS = [
-#    "http://localhost:3000",
-#    "http://127.0.0.1:9000",
-#    "http://192.168.1.116",
-#]
-
 
 ROOT_URLCONF = 'b2b.urls'
 
@@ -149,20 +133,16 @@ SWAGGER_SETTINGS = {
         }
     },
     "JSON_EDITOR" : True,
-    # "DOC_EXPANSION" : "full",
     "LOGIN_URL" : 'rest_framework:login',
     "LOGOUT_URL" : 'rest_framework:logout',
     "DEFAULT_MODEL_RENDERING":"example"
-    # "OPERATIONS_SORTER" : "method",
 }
-
 
 # Email config
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bigjohnsistemas@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Bigjohn2020*bj*'
 EMAIL_HOST_PASSWORD = 'dkkkwfrsabgavlou'
 
 # Internationalization
